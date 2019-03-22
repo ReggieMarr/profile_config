eaabd17fbdf12bad9d869a717b7bab53ea67dafe
reggiemarr <reginald.t.marr@gmail.com>
2019-03-21 23:09:45 -0400

    First commit

---
+++ customscripts/typist/src/main.rs
@@ -0,0 +1,16 @@
+use std::env::args
+
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf
+}
+
+fn main() {
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no pattern given");
+    let thisCli = Cli {
+        pattern: pattern,
+        path: path
+    };
+    
+}
